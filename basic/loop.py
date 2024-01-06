fe0c6814fe512977567c2cacfd7531341ffd785f
SRAKIB17 <rakibulssc5@gmail.com>
2024-01-06 14:10:32 +0600

    'doen

--- basic/loop.py
+++ basic/loop.py
@@ -1,2 +1,30 @@
+x = "PYTHON"
+# left to right
+# 0, 1, 2,3,4.................
+# right to left
+# -1, -2, -3, -4 .........
+
+# print(x[-1])
+# for i in x:
+#     if (i == 'H'):
+#         break
+#     print(i)
+# count = 1
+# while (count):
+#     count += 1
+#     if (count == 100):
+#         break
+#     elif (count % 2 == 0):
+#         continue
+#     print(count)
+
+#     # code execute
 
 
+def function_name(name):
+    return "Hello, "+name
+
+
+print("line28", function_name("python"))
+
+# STRING, SET, Tuple, list, array, frozenset, dictionary
